9e12b48721c1dfc676654bdc37de15511cdb2b2c
alfaraby <alfaraby@detik.com>
2022-12-18 17:18:10 +0700

    Initial Commit

---
+++ AppFastlane/AppFastlaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppFastlaneApp.swift
+//  AppFastlane
+//
+//  Created by Muhammad Alfaraby on 18/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AppFastlaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
