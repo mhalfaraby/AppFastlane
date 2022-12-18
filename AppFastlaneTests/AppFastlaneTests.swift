43a36083bfa3b10964925c93edff7c99d21e8e41
alfaraby <alfaraby@detik.com>
2022-12-18 17:42:24 +0700

    add unit test

---
+++ AppFastlaneTests/AppFastlaneTests.swift
@@ -0,0 +1,15 @@
+//
+//  AppFastlaneTests.swift
+//  AppFastlaneTests
+//
+//  Created by Muhammad Alfaraby on 18/12/22.
+//
+
+import XCTest
+
+final class AppFastlaneTests: XCTestCase {
+
+    func testExample() throws {
+        XCTAssertEqual(2*2, 4)
+    }
+}
